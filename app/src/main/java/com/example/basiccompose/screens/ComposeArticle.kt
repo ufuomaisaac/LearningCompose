5db26c37ac1de727765210ddf3b95c031085f8f2
ufuomaisaac <ejemudaroufuoma@gmail.com>
2023-10-09 14:30:30 +0100

    compose_article

---
+++ app/src/main/java/com/example/basiccompose/screens/ComposeArticle.kt
@@ -0,0 +1,51 @@
+package com.example.basiccompose.screens
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.basiccompose.Greeting
+import com.example.basiccompose.R
+import com.example.basiccompose.ui.theme.BasicComposeTheme
+
+@Composable
+fun ComposeArticle(modifier: Modifier = Modifier){
+    Column {
+        Image(painter = painterResource(id = R.drawable.bg_compose_background),
+            contentDescription = "Artical image",
+            modifier = Modifier.fillMaxWidth())
+
+        Text(
+            text = "Jetpack Compose tutorial",
+            modifier.padding(16.dp),
+            fontSize = 24.sp
+        )
+        Text(
+            text = stringResource(id = R.string.first_text),
+            modifier.padding(16.dp)
+            )
+
+        Text(
+            text = stringResource(id = R.string.second_text),
+            modifier.padding(16.dp)
+        )
+    }
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    BasicComposeTheme {
+        ComposeArticle()
+    }
+}
