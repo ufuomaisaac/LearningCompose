e5cd42ca271dea448dd115e9d41ab3864498221c
ufuomaisaac <ejemudaroufuoma@gmail.com>
2023-12-04 01:34:53 +0100

    basic_layout navigation_rail

--- app/src/main/java/com/example/basiccompose/screens/BasicLayout/SearchBar.kt
+++ app/src/main/java/com/example/basiccompose/screens/BasicLayout/SearchBar.kt
@@ -9,6 +9,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.heightIn
@@ -23,12 +24,25 @@ import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.lazy.grid.LazyHorizontalGrid
 //import androidx.compose.foundation.lazy.grid.
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.Search
+import androidx.compose.material.icons.filled.Share
+import androidx.compose.material.icons.filled.ShoppingCart
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButtonDefaults
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.NavigationBarItemColors
+import androidx.compose.material3.NavigationRail
+import androidx.compose.material3.NavigationRailItem
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
@@ -38,6 +52,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.drawWithContent
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
@@ -47,6 +62,8 @@ import com.example.basiccompose.R
 import com.example.basiccompose.ui.theme.BasicComposeTheme
 import com.example.basiccompose.ui.theme.md_theme_light_onTertiary
 import com.example.basiccompose.ui.theme.typography
+
+
 
 
 
@@ -103,8 +120,7 @@ fun SearchBar(
     modifier: Modifier = Modifier
 ){
     Column(modifier =
-    modifier.padding(horizontal = 16.dp)
-        .padding(top = 16.dp)) {
+    modifier.padding(horizontal = 16.dp)) {
         TextField(
             value = " ",
             leadingIcon = {
@@ -248,9 +264,12 @@ fun HomeSection(
 }
 
 @Composable
-fun HomeScreen(modifier: Modifier = Modifier) {
+fun HomeScreen(modifier: Modifier = Modifier.padding(top = 16.dp)) {
 
-    Column {
+    Column(
+        modifier = modifier
+            .verticalScroll(rememberScrollState())) {
+        //Spacer(modifier = Modifier.height(16.dp))
         SearchBar()
         HomeSection(title = R.string.align_your_body) {
             AlignYourBodyRow()
@@ -259,16 +278,120 @@ fun HomeScreen(modifier: Modifier = Modifier) {
         HomeSection(title = R.string.favorite_collections) {
             FavoriteCollectionsGrid()
         }
+        Spacer(modifier = Modifier.height(16.dp))
+
     }
 
 }
 
+@Composable
+fun BottomNavigation(modifier: Modifier = Modifier) {
+    NavigationBar {
+        NavigationBarItem(
+            icon = {
+                Icon(
+                    imageVector = Icons.Default.ShoppingCart,
+                    contentDescription = null
+                )
+            },
+            label = {
+                Text(
+                    text = stringResource(R.string.bottom_navigation_home)
+                )
+            },
+            selected = true,
+            onClick = {}
+        )
+        NavigationBarItem(
+            icon = {
+                Icon(
+                    imageVector = Icons.Default.AccountCircle,
+                    contentDescription = null
+                )
+            },
+            label = {
+                Text(
+                    text = stringResource(R.string.bottom_navigation_profile)
+                )
+            },
+            selected = false,
+            onClick = {}
+        )
+    }
+}
 
-@Preview(showBackground = true)
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun AppPortrait() {
+    BasicComposeTheme{
+        Scaffold(
+            bottomBar = { BottomNavigation() }
+        ) { padding ->
+            HomeScreen(Modifier.padding(padding))
+        }
+    }
+}
+
+
+@Composable
+fun AppNavigationRail(modifier: Modifier = Modifier) {
+    NavigationRail(
+        modifier = modifier.padding(start = 8.dp, end = 8.dp),
+        containerColor = MaterialTheme.colorScheme.background) {
+
+        Column(modifier = modifier.fillMaxHeight(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally) {
+
+            NavigationRailItem(
+                selected = true,
+                onClick = {},
+                icon = {
+                    Icon(
+                        imageVector = Icons.Default.AccountCircle,
+                        contentDescription = null)
+                },
+                label = {
+                    Text(text = stringResource(id = R.string.bottom_navigation_profile))
+                },
+            )
+            NavigationRailItem(
+                selected = false,
+                onClick = {},
+                icon = {
+                    Icon(
+                        imageVector = Icons.Default.Home,
+                        contentDescription = null
+                    )
+                },
+                label = {
+                    Text(stringResource(R.string.bottom_navigation_home))
+                })
+
+        }
+
+    }
+}
+
+
+@Composable
+fun AppLandScape(){
+        Row {
+            AppNavigationRail()
+            HomeScreen()
+        }
+}
+
+@Composable
+fun MyApp() {
+
+}
+
+@Preview(showBackground = true, heightDp = 400, widthDp = 800)
 @Composable
 fun HomeSectionPreview() {
     BasicComposeTheme {
-        HomeScreen()
+        AppLandScape()
     }
 }
 @Preview(showBackground = true, backgroundColor = 0xFFF5F0EE)
