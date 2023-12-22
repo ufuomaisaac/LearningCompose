7a900dab83f6a4e7ac64a69b7277586488426783
ufuomaisaac <ejemudaroufuoma@gmail.com>
2023-12-22 10:04:14 +0100

    motion scene 0.2

--- app/src/main/java/com/example/basiccompose/MainActivity.kt
+++ app/src/main/java/com/example/basiccompose/MainActivity.kt
@@ -3,30 +3,47 @@ package com.example.basiccompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Slider
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.windowsizeclass.ExperimentalMaterial3WindowSizeClassApi
 import androidx.compose.material3.windowsizeclass.WindowWidthSizeClass
 import androidx.compose.material3.windowsizeclass.calculateWindowSizeClass
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.layoutId
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.constraintlayout.compose.ConstraintLayout
+import androidx.constraintlayout.compose.ConstraintLayoutTagParentData
+import androidx.constraintlayout.compose.ConstraintSet
 import com.example.basiccompose.screens.ArtSpace.ArtWall
 import com.example.basiccompose.screens.BasicLayout.HomeScreen
 import com.example.basiccompose.screens.BasicLayout.MySootheApp
 //import com.example.basiccompose.screens.ArtSpace.ArtWall
 import com.example.basiccompose.screens.ComposeArticle
 import com.example.basiccompose.screens.ComposeQuadrant
+import com.example.basiccompose.screens.ConstraintLayoutContent
 import com.example.basiccompose.screens.DiceRollerApp
 import com.example.basiccompose.screens.IndianFlagScreen
 //import com.example.basiccompose.screens.DiceRollerApp
 import com.example.basiccompose.screens.LemonButtonAndText
 import com.example.basiccompose.screens.MyApp
+import com.example.basiccompose.screens.ProfileHeader
 import com.example.basiccompose.screens.TaskManager
 import com.example.basiccompose.screens.TipTimeLayout
 import com.example.basiccompose.ui.theme.BasicComposeTheme
@@ -53,10 +70,49 @@ class MainActivity : ComponentActivity() {
                     //LemonButtonAndText()
                     //TipTimeLayout()
                     //ArtWall()
-                    IndianFlagScreen()
+                    //IndianFlagScreen()
                     //HomeScreen()
                     //MySootheApp(windowSize = windowSizeClass)
                     //onBoardingScreen(onContinueClicked = {})
+                    var progress by remember {
+                        mutableStateOf(0f)
+                    }
+
+                        val constraintSet = ConstraintSet() {
+                            val profileHeader = createRefFor("profile_header")
+                            val slider = createRefFor("slider")
+
+                            constrain(profileHeader) {
+                                top.linkTo(parent.top)
+                                start.linkTo(parent.start)
+                                end.linkTo(parent.end)
+                                bottom.linkTo(slider.top, margin = 48.dp)
+                            }
+
+                            constrain(slider) {
+                                top.linkTo(parent.top)
+                                bottom.linkTo(parent.bottom)
+                                start.linkTo(parent.start)
+                                end.linkTo(parent.end)
+                        }
+                    }
+                        ConstraintLayout(
+                            constraintSet = constraintSet,
+                            modifier = Modifier
+                                .fillMaxSize()
+                                .background(Color.White)
+                                .padding(16.dp)) {
+
+
+                            ProfileHeader(
+                                progress = progress,
+                                modifier = Modifier.layoutId("profile_header"))
+
+                            Slider(value = progress,
+                                onValueChange = { progress = it },
+                                modifier = Modifier.layoutId("slider")
+                            )
+                        }
                 }
             }
         }
--- app/src/main/java/com/example/basiccompose/screens/ProfileHeader.kt
+++ app/src/main/java/com/example/basiccompose/screens/ProfileHeader.kt
@@ -2,24 +2,33 @@ package com.example.basiccompose.screens
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.layout
 import androidx.compose.ui.layout.layoutId
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.constraintlayout.compose.ExperimentalMotionApi
 import androidx.constraintlayout.compose.MotionLayout
 import androidx.constraintlayout.compose.MotionScene
 import com.example.basiccompose.R
+import com.example.basiccompose.ui.theme.BasicComposeTheme
 
 @OptIn(ExperimentalMotionApi::class)
 @Composable
-fun ProfileHeader(progress : Float) {
+fun ProfileHeader(progress : Float, modifier: Modifier) {
     val context = LocalContext.current
     val motionScene = remember {
         context.resources
@@ -34,10 +43,31 @@ fun ProfileHeader(progress : Float) {
     ) {
         Box(modifier = Modifier
             .fillMaxSize()
-            .background(Color.Cyan)
+            .background(Color.DarkGray)
             .layoutId("box"))
 
-       // Image(painter = R.drawable, contentDescription = )
-    }
+       Image(painter = painterResource(id = R.drawable.ab1_inversions),
+           contentDescription = "profile_picture",
+           modifier = Modifier
+               .layoutId("profile_pic")
+               .clip(CircleShape)
+               .border(
+                   width = 2.dp,
+                   color = Color.Green,
+                   shape = CircleShape
+               ))
 
+        Text(text = "Ufuoma Isaac",
+            fontSize = 24.sp,
+            modifier = Modifier.layoutId("username"))
+
+    }
+}
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview11() {
+    BasicComposeTheme {
+       // ProfileHeader(progress = 1f)
+        // Greetings()
+    }
 }
