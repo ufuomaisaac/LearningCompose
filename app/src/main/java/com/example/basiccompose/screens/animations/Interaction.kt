04b660584bf0b7e3c5fe6a6ac099bef465db6ba8
ufuomaisaac <ejemudaroufuoma@gmail.com>
2024-07-22 23:56:24 +0100

    offsets

--- app/src/main/java/com/example/basiccompose/screens/animations/Interaction.kt
+++ app/src/main/java/com/example/basiccompose/screens/animations/Interaction.kt
@@ -1,23 +1,30 @@
 package com.example.basiccompose.screens.animations
 
 import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.animateIntOffsetAsState
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.interaction.collectIsPressedAsState
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
+import kotlin.math.roundToInt
 
 @Composable
 fun Interaction() {
@@ -46,8 +53,43 @@ fun Interaction() {
     }
 }
 
+@Composable
+fun Displacement() {
+
+    var moved by remember { mutableStateOf(false)}
+    val  pxToMove = with(LocalDensity.current) {
+        100.dp.toPx().roundToInt()
+    }
+
+    val offset by animateIntOffsetAsState(
+        targetValue = if (moved) {
+            IntOffset(pxToMove, pxToMove)
+        } else {
+            IntOffset(50, 50)
+        },
+        label = "offset"
+    )
+
+    Box(modifier = Modifier.
+            offset{
+                offset
+            }
+        .background(Color.Blue)
+        .size(100.dp)
+        .clickable(
+            interactionSource = remember { MutableInteractionSource() },
+            indication = null
+        ) {
+            moved = !moved
+        } )
+
+}
+
+
 @Preview
 @Composable
 fun preview() {
-    Interaction()
+    Displacement()
 }
+
+
