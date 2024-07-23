878c740a9fae3b89b5fa5049855bfcf66e9f3ea4
ufuomaisaac <ejemudaroufuoma@gmail.com>
2024-07-23 10:01:53 +0100

    Text Infinite Animation

--- app/src/main/java/com/example/basiccompose/screens/animations/Interaction.kt
+++ app/src/main/java/com/example/basiccompose/screens/animations/Interaction.kt
@@ -1,8 +1,16 @@
 package com.example.basiccompose.screens.animations
 
+import androidx.compose.animation.core.Easing
+import androidx.compose.animation.core.RepeatMode
 import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.animateFloat
 import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.animation.core.animateIntOffsetAsState
+import androidx.compose.animation.core.animateValue
+import androidx.compose.animation.core.infiniteRepeatable
+import androidx.compose.animation.core.rememberInfiniteTransition
+import androidx.compose.animation.core.repeatable
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
@@ -14,16 +22,22 @@ import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.LocalTextStyle
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.TransformOrigin
 import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.style.TextMotion
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
@@ -119,10 +133,40 @@ fun PaddingAnItem() {
 }
 
 
+@Composable
+fun TextInfiniteAnimation() {
+    var infiniteTransition = rememberInfiniteTransition(label = "infinite transition")
+    val scale by infiniteTransition.animateFloat(
+        initialValue = 2f,
+        targetValue = 8f,
+        animationSpec = infiniteRepeatable(tween(1000, easing = Easing { 3f * it }, delayMillis = 1000),RepeatMode.Reverse),
+        label = "scale"
+    )
+
+    Box(modifier = Modifier
+        .fillMaxSize()
+        .background(color = Color.Black)) {
+        Text(
+            text = "Hello",
+            modifier = Modifier.graphicsLayer {
+                scaleX =scale
+                scaleY = scale
+                transformOrigin = TransformOrigin.Center
+            }
+                .align(Alignment.Center),
+            color = Color.Green,
+
+            style = LocalTextStyle.current.copy(textMotion = TextMotion.Animated)
+
+        )
+    }
+}
+
+
 @Preview
 @Composable
 fun preview() {
-    PaddingAnItem()
+    TextInfiniteAnimation()
 }
 
 
